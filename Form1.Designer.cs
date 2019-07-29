138fcffcdc217083e9d15c76c685fcc2b43741c6
2450945998 <2450945998@qq.com>
2019-07-29 15:05:28 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace 百度离线
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("团一");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("旅一", new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("团二");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("旅二", new System.Windows.Forms.TreeNode[] {
+            treeNode3});
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("军一", new System.Windows.Forms.TreeNode[] {
+            treeNode2,
+            treeNode4});
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("团三");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("团四");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("旅三", new System.Windows.Forms.TreeNode[] {
+            treeNode6,
+            treeNode7});
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("军二", new System.Windows.Forms.TreeNode[] {
+            treeNode8});
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "团一",
+            "3",
+            "行进",
+            "2019/7/20 15.20.00",
+            "(118.793,32.029)",
+            "一",
+            "正常"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "团二",
+            "4",
+            "行进",
+            "2019/7/20 15.20.00",
+            "(118.790,32.100)",
+            "二",
+            "异常"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+            "团三",
+            "3",
+            "行进",
+            "2019/7/20 15.20.00",
+            "(118.863,32.031)",
+            "三",
+            "正常"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem(new string[] {
+            "团四",
+            "0",
+            "修整",
+            "2019/7/20 15.20.00",
+            "(118.793,32.000)",
+            "四",
+            "异常"}, -1);
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.队伍 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.速度 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.状态 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.时间 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.位置 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.长官 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.通信状态 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(800, 397);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("E:\\C#\\百度离线\\bin\\Debug\\self1.html", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WebBrowser1_DocumentCompleted);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.treeView1.Location = new System.Drawing.Point(692, 0);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "团一";
+            treeNode1.Text = "团一";
+            treeNode2.Name = "旅一";
+            treeNode2.Text = "旅一";
+            treeNode3.Name = "团二";
+            treeNode3.Text = "团二";
+            treeNode4.Name = "旅二";
+            treeNode4.Text = "旅二";
+            treeNode5.Name = "军一";
+            treeNode5.Text = "军一";
+            treeNode6.Name = "团三";
+            treeNode6.Text = "团三";
+            treeNode7.Name = "团四";
+            treeNode7.Text = "团四";
+            treeNode8.Name = "旅三";
+            treeNode8.Text = "旅三";
+            treeNode9.Name = "军二";
+            treeNode9.Text = "军二";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode9});
+            this.treeView1.Size = new System.Drawing.Size(108, 153);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.BeforeCollapse += new System.Windows.Forms.TreeViewCancelEventHandler(this.TreeView1_BeforeCollapse);
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.TreeView1_BeforeExpand);
+            this.treeView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.TreeView1_MouseDoubleClick);
+            this.treeView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TreeView1_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(659, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "→";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "↓";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.队伍,
+            this.速度,
+            this.状态,
+            this.时间,
+            this.位置,
+            this.长官,
+            this.通信状态});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listView1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listView1.GridLines = true;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4});
+            this.listView1.Location = new System.Drawing.Point(0, 276);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(800, 121);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.Visible = false;
+            // 
+            // 队伍
+            // 
+            this.队伍.Text = "队伍";
+            // 
+            // 速度
+            // 
+            this.速度.Text = "速度(km/h)";
+            this.速度.Width = 112;
+            // 
+            // 状态
+            // 
+            this.状态.Text = "状态";
+            this.状态.Width = 89;
+            // 
+            // 时间
+            // 
+            this.时间.Text = "时间";
+            this.时间.Width = 167;
+            // 
+            // 位置
+            // 
+            this.位置.Text = "位置";
+            this.位置.Width = 154;
+            // 
+            // 长官
+            // 
+            this.长官.Text = "长官";
+            this.长官.Width = 74;
+            // 
+            // 通信状态
+            // 
+            this.通信状态.Text = "通信状态";
+            this.通信状态.Width = 98;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.textBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(692, 143);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(108, 118);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(692, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "收起";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.Timer3_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 197);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "评估";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(0, 220);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "导控";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 397);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColumnHeader 队伍;
+        private System.Windows.Forms.ColumnHeader 速度;
+        private System.Windows.Forms.ColumnHeader 状态;
+        private System.Windows.Forms.ColumnHeader 时间;
+        private System.Windows.Forms.ColumnHeader 位置;
+        private System.Windows.Forms.ColumnHeader 长官;
+        private System.Windows.Forms.ColumnHeader 通信状态;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Net;
+
+
+
+namespace 百度离线
+{
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    [ComVisibleAttribute(true)]
+
+
+    public partial class Form1 : Form
+    {
+        public int i = 0;
+        public int mouse = 0;
+        public int mouse1 = 0;
+       
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = false;
+            timer1.Interval = 4000;
+            timer1.Enabled = true;
+
+           
+        }
+
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string str_url = Application.StartupPath + "\\self1.html";
+            Uri url = new Uri(str_url);
+            webBrowser1.Url = url; 
+            
+            webBrowser1.ObjectForScripting = this;
+            webBrowser1.ScriptErrorsSuppressed = true;
+            
+            
+
+
+
+
+            int x3 = this.ClientSize.Width - button1.Size.Width;
+            int y3 = 0;
+            Point p3 = new Point(x3, y3);
+            button1.Location = p3;
+            button1.Text = "←";
+
+            int x4 = 0;
+            int y4 = this.ClientSize.Height - button2.Size.Height;
+            Point p4 = new Point(x4, y4);
+            button2.Location = p4;
+            button2.Text = "↑";
+
+            int x5 = treeView1.Location.X;
+            int y5 = treeView1.Location.Y + treeView1.Size.Height;
+            Point p5 = new Point(x5, y5);
+            textBox1.Location = p5;
+
+            listView1.Height = ClientSize.Height * 4 / 21;
+            listView1.Width = ClientSize.Width;
+
+            treeView1.Height = ClientSize.Height * 3 / 7;
+            treeView1.Width = ClientSize.Width * 1 / 4;
+
+            textBox1.Height = ClientSize.Height * 8 / 21;
+
+            webBrowser1.Height = ClientSize.Height;
+            webBrowser1.Width = ClientSize.Width;
+
+            int x7 = this.ClientSize.Width - button1.Size.Width;
+            int y7 = 0;
+            Point p7 = new Point(x7, y7);
+            button1.Location = p7;
+            button1.Text = "←";
+            treeView1.Visible = false;
+
+            int x8 = 0;
+            int y8 = this.ClientSize.Height - button2.Size.Height;
+            Point p8 = new Point(x8, y8);
+            button2.Location = p8;
+            button2.Text = "↑";
+            listView1.Visible = false;
+
+            int x9 = treeView1.Location.X;
+            int y9 = treeView1.Location.Y + treeView1.Size.Height;
+            Point p9 = new Point(x9, y9);
+            textBox1.Location = p9;
+
+            int x6 = treeView1.Location.X;
+            int y6 = y5 + textBox1.Size.Height ;
+            Point p6 = new Point(x6, y6);
+            button3.Location = p6;
+            textBox1.Visible = false;
+            button3.Visible = false;
+        }
+
+        private void TreeView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.mouse = e.Clicks;
+        }
+
+        private void TreeView1_BeforeCollapse(object sender, TreeViewCancelEventArgs e)
+        {
+            if (this.mouse > 1)
+                e.Cancel = true;
+            else
+            {
+                e.Cancel = false;
+            }
+        }
+
+        private void TreeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            if (this.mouse > 1)
+                e.Cancel = true;
+            else
+            {
+                e.Cancel = false;
+            }
+        }
+
+        private void TreeView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (treeView1.SelectedNode.Name == "军一")
+            {
+                button3.Visible = true;
+                textBox1.Visible = true;
+                textBox1.Text = ("军长：一 \r\n\r\n人数：100 \r\n\r\n弹药：130（一般）");
+            }
+
+            if (treeView1.SelectedNode.Name == "军二")
+            {
+                button3.Visible = true;
+                textBox1.Visible = true;
+                textBox1.Text = ("军长：二 \r\n\r\n人数：80 \r\n\r\n弹药：90（充足）");
+            }
+
+            if (treeView1.SelectedNode.Name == "旅一")
+            {
+                button3.Visible = true;
+                textBox1.Visible = true;
+                textBox1.Text = ("旅长：一 \r\n\r\n人数：50 \r\n\r\n弹药：30（不足）");
+            }
+
+            if (treeView1.SelectedNode.Name == "旅二")
+            {
+                button3.Visible = true;
+                textBox1.Visible = true;
+                textBox1.Text = ("旅长：二 \r\n\r\n人数：50 \r\n\r\n弹药：100（充足）");
+            }
+
+            if (treeView1.SelectedNode.Name == "旅三")
+            {
+                button3.Visible = true;
+                textBox1.Visible = true;
+                textBox1.Text = ("旅长：三 \r\n\r\n人数：60 \r\n\r\n弹药：50（一般）");
+            }
+
+            if (treeView1.SelectedNode.Name == "团一")
+            {
+                button3.Visible = true;
+                textBox1.Visible = true;
+                textBox1.Text = ("团长：一 \r\n\r\n人数：35 \r\n\r\n弹药：30（充足）");
+            }
+
+            if (treeView1.SelectedNode.Name == "团二")
+            {
+                button3.Visible = true;
+                textBox1.Visible = true;
+                textBox1.Text = ("团长：二 \r\n\r\n人数455 \r\n\r\n弹药：90（充足）");
+            }
+
+            if (treeView1.SelectedNode.Name == "团三")
+            {
+                button3.Visible = true;
+                textBox1.Visible = true;
+                textBox1.Text = ("团长：三 \r\n\r\n人数：35 \r\n\r\n弹药：20（不足）");
+            }
+
+            if (treeView1.SelectedNode.Name == "团四")
+            {
+                button3.Visible = true;
+                textBox1.Visible = true;
+                textBox1.Text = ("团长：四 \r\n\r\n人数：30 \r\n\r\n弹药：10（不足）");
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "←")
+            {
+                treeView1.Visible = true;
+                button1.Text = "→";
+                int x1 = this.ClientSize.Width - textBox1.Size.Width - button1.Size.Width;
+                int y1 = 0;
+                Point p1 = new Point(x1, y1);
+                button1.Location = p1;
+
+
+            }
+            else
+            {
+                treeView1.Visible = false;
+                button1.Text = "←";
+                int n1 = this.ClientSize.Width - button1.Size.Width;
+                int m1 = 0;
+                Point p1_1 = new Point(n1, m1);
+                button1.Location = p1_1;
+                button3.Visible = false;
+                textBox1.Visible = false;
+
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "↑")
+            {
+                listView1.Visible = true;
+                button2.Text = "↓";
+                int x2 = 0;
+                int y2 = this.ClientSize.Height - listView1.Size.Height - 20;
+                Point p2 = new Point(x2, y2);
+                button2.Location = p2;
+            }
+            else
+            {
+                listView1.Visible = false;
+                button2.Text = "↑";
+                int n2 = 0;
+                int m2 = this.ClientSize.Height - button2.Size.Height;
+                Point p2_1 = new Point(n2, m2);
+                button2.Location = p2_1;
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            button3.Visible = false;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            listView1.Height = ClientSize.Height * 4 / 21;
+            listView1.Width = ClientSize.Width;
+
+            treeView1.Height = ClientSize.Height * 3 / 7;
+            treeView1.Width = ClientSize.Width * 1 / 4;
+
+            textBox1.Height = ClientSize.Height * 8 / 21;
+
+            webBrowser1.Height = ClientSize.Height;
+            webBrowser1.Width = ClientSize.Width;
+
+            int x3 = this.ClientSize.Width - button1.Size.Width;
+            int y3 = 0;
+            Point p3 = new Point(x3, y3);
+            button1.Location = p3;
+            button1.Text = "←";
+            treeView1.Visible = false;
+
+            int x4 = 0;
+            int y4 = this.ClientSize.Height - button2.Size.Height;
+            Point p4 = new Point(x4, y4);
+            button2.Location = p4;
+            button2.Text = "↑";
+            listView1.Visible = false;
+
+            int x5 = treeView1.Location.X;
+            int y5 = treeView1.Location.Y + treeView1.Size.Height;
+            Point p5 = new Point(x5, y5);
+            textBox1.Location = p5;
+
+            int x6 = treeView1.Location.X;
+            int y6 = y5 + textBox1.Size.Height - button3.Size.Height;
+            Point p6 = new Point(x6, y6);
+            button3.Location = p6;
+            textBox1.Visible = false;
+            button3.Visible = false;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            
+
+            //webBrowser1.Document.InvokeScript("getpoint");
+
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
